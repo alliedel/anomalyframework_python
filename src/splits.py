feeb0ccd3aaa4d8f485a21166088c09bfe56af28
Allie <allie@skydio.com>
2016-09-26 18:28:17 -0700

    in the middle of implementing splits.py

---
+++ src/splits.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def generate_splits(window_stride_mult, window_size, num_points, num_shuffles, outfile):
+    """ Generates the splits per permuted set of points """
+
+    stride = window_stride_mult * window_size
+    if divmod(window_size, stride):
+        raise ValueError('Window stride must be divisible by window size. Change '
+                         'window_stride_mult')
+    first_idx = window_size
+    last_idx = num_points - 2 * window_size + 1 + window_size
+    interval_start_idxs = np.arange(start=first_idx, stop=last_idx, step=stride)
+
+    interval_end_idxs = interval_start_idxs + window_size - 1
+
+    num_intervals = len(interval_start_idxs)
+
+    if num_intervals < 1:
+        raise ValueError('Window size should be < %d == # of frames.\n' % num_points)
+
+    num_splits = num_intervals * (num_shuffles + 1)
+    ys = np.nan(num_splits, num_points)
+
+    for i in range(interval_start_idxs):
+        ys[i, np.arange(interval_start_idxs[i], interval_end_idxs[i]+1)] = 1
+        ys[i, range(interval_start_idxs[i])] = 0
+
+    for shuffle_idx in range(num_shuffles):
+        
