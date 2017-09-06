f5736ff7d5ba95dd2e33342292c3a8117e8dc10f
Allie Del Giorno <adelgior@cs.cmu.edu>
2017-09-06 09:03:28 -0400

    modificiations to analysis.py

--- analysis.py
+++ analysis.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import pickle
 import scipy.ndimage
+import scipy.signal
+import display
 
 # Load the FOCUS packageimport sys
 import sys
@@ -11,76 +13,82 @@ sys.path.append('/home/allie/projects/focus')  # Just to remember where this pat
 # from focus import python.features
 from python import features
 
-
 IM_DIR = '/home/allie/workspace/images'
 
 
-def save_fig_to_workspace(filename=None, workspace_dir=IM_DIR):
-    if not filename:
-        filename = '%02d.png' % plt.gcf().number
-    plt.savefig(os.path.join(workspace_dir, filename))
+def apply_averaging_filter(x, filter_size=5):
+    return np.convolve(signal, np.ones(filter_size,) / float(filter_size), mode='valid')
 
 
-def save_all_figs_to_workspace():
-    for i in plt.get_fignums():
-        plt.figure(i)
-        save_fig_to_workspace()
+def apply_median_filter(x, filter_size=5):
+    return scipy.signal.medfilt(x, filter_size)
+
 
 if __name__ == '__main__':
-    results_dirs = sorted(glob.glob('data/results/2017_02_06/*'))
-    results_dirs = results_dirs[:-1]  # last one is corrupted from memory
+    results_dirs = sorted(glob.glob('/home/allie/workspace/server_sync/*/*'))
     fignum = 0
     plt.close('all')
+    anomalousness_to_save = []
+    pars_to_save = []
     for results_dir in reversed(results_dirs):
+        fignum+=1
+        print(results_dir)
+        print(os.path.join(results_dir, 'anomaly_ratings.npy'))
         try:
             anomalousness = np.load(os.path.join(results_dir, 'anomaly_ratings.npy'))
-            anomalousness = abs(anomalousness - 0.5)
-            anomalousness = anomalousness/(1.0-anomalousness)
-            pars = pickle.load(open(os.path.join(results_dir, 'pars.pickle'), 'r'))
-            is_whitened = 'whiten' in os.path.basename(pars.paths.files.infile_features)
-            if is_whitened:
-                feats_file = pars.paths.files.infile_features.replace('_self_whiten','')
-            else:
-                feats_file = pars.paths.files.infile_features
-            locs3 = np.load(feats_file.replace('.train', '.npy').replace(
-                'raw._','raw_locs3._'))
-            feature_pars = np.load(feats_file.replace('.train',
-                                                                           '.npy').replace(
-                'raw._','raw_pars._'))
-            coords_list = np.load(feats_file.replace('.train',
-                                                                            '.npy').replace(
-                'raw._','raw_grid_coords_list._'))
-            rs, cs, ts = coords_list[0], coords_list[1], coords_list[2]
-
-            # TODO(allie) : scoreanomalies doesn't handle the zero-indexing case.
-            assert locs3.shape[0] - len(anomalousness) <= 1
-            locs3 = locs3[1:,:]
-            cs = range(16)
-            anomaly_ratings3 = np.zeros((len(rs), len(cs), len(ts)))
-            # import ipdb; ipdb.set_trace()
-            for anomaly_index in range(locs3.shape[0]):
-                anomaly_ratings3[locs3[anomaly_index,0], locs3[anomaly_index,1],
-                                 locs3[anomaly_index,2]] = anomalousness[anomaly_index]
-            fignum += 1
-            plt.figure(fignum); plt.clf()
-            smooth_size = 5
-            smoothed_ratings3 = scipy.ndimage.uniform_filter(anomaly_ratings3,
-                                                           smooth_size)
-            smoothed_by_frame = scipy.ndimage.uniform_filter(anomaly_ratings3,
-                                                           smooth_size).sum(axis=1).sum(axis=0)
-            smoothed_signal = smoothed_by_frame.reshape(-1,)
-            signal = smoothed_signal
-            # signal = anomaly_ratings3.sum(axis=1).sum(axis=0)
-            # signal = anomaly_ratings3.reshape(-1,)
-            plt.fill_between(range(len(signal)), signal, facecolor='#5D8AA8', alpha=0.5)
-            videonum_as_str = os.path.basename(feats_file)[0:2]
+            signal = anomalousness/(1.0-anomalousness)
+            # Smooth temporally
+            signal = apply_averaging_filter(signal, 100)
+            pars = pickle.load(open(os.path.join(results_dir, 'pars.pickle'), 'rb'))
+            feats_file = pars.paths.files.infile_features
+            plt.figure(fignum)
+            plt.fill_between(range(len(signal)), signal, facecolor=display.GOOD_COLOR_CYCLE[0], alpha=1.0) # alpha=0.5
+            signal_sorted = np.sort(signal)
+            bottom_ninetyfive_percent = signal_sorted[:int(np.floor(len(signal_sorted) * 0.95))]
+            y_max = np.median(bottom_ninetyfive_percent) + 3*np.std(bottom_ninetyfive_percent)
+            plt.ylim([0, y_max])
+            videonum_as_str = os.path.basename(feats_file)
             lambd = pars.algorithm.discriminability.lambd
-            plt.title('video: {}\nlambda: {}\nis_whitened: {}'.format(videonum_as_str, lambd,
-                                                                   is_whitened))
-            print('Saving figure to {}'.format(plt.gcf().number))
-            save_fig_to_workspace()
+            max_buffer_size = pars.algorithm.discriminability.max_buffer_size
+            title = 'video: {}\nlambda: {}\nmax_buffer_size:{}'.format(videonum_as_str, lambd, max_buffer_size)
+            plt.title(title)
+            print('Saving figure to {}.png in workspace'.format(plt.gcf().number))
+            display.save_fig_to_workspace()
+            results_figure_name = os.path.join(results_dir, 'anomaly_rating.png')
+            display.savefig(results_figure_name)
+            print('Saving figure to {}'.format(results_figure_name))
+            thresholded_anom_results = (signal > (np.median(signal) + 2 * np.std(bottom_ninetyfive_percent))).astype(float) * signal 
+            plt.clf()
+            plt.fill_between(range(len(thresholded_anom_results)), thresholded_anom_results, facecolor=display.GOOD_COLOR_CYCLE[1], alpha=1.0, label='anomalous: {:.4g}%'.format(100.0 * np.sum(thresholded_anom_results > 0) / len(thresholded_anom_results)))
+            plt.legend()
+            plt.ylim([0, y_max])
+            plt.title(title)
+            print('Saving figure to {}'.format(results_figure_name.replace('rating', 'rating_thresholded')))
+            display.savefig(results_figure_name.replace('rating', 'rating_thresholded'))
+            if videonum_as_str.find('1101') != -1 and lambd == 10:
+                print('results_dir of interest: {}'.format(results_dir))
+                anomalousness_to_save += [anomalousness]
+                pars_to_save += [pars]
+                anomalous_frames = [os.path.join('/home/allie/projects/aladdin/videos/LGW_20071101_E1_CAM1frames', 'image-%06d' % frame_num + '.png') for frame_num in np.where(thresholded_anom_results > 0)[0]]
+                destination_frames = [os.path.join('/home/allie/workspace/etc/1101_results', 'image-%06d' % frame_num + '.png') for frame_num in np.where(thresholded_anom_results > 0)[0]]
+                for src, dest in zip(anomalous_frames, destination_frames):
+                    import shutil;
+                    shutil.copyfile(src, dest)
+                
+            video_id = '1108'
+            if videonum_as_str.find(video_id) != -1 and lambd == 10:
+                print('results_dir of interest: {}'.format(results_dir))
+                anomalousness_to_save += [anomalousness]
+                pars_to_save += [pars]
+                desination_dir = '/home/allie/workspace/etc/{}_results'.format(video_id)
+                os.mkdir(destination_dir)
+                anomalous_frames = [os.path.join('/home/allie/projects/aladdin/videos/LGW_2007{}_E1_CAM1frames'.format(video_id), 'image-%06d' % frame_num + '.png') for frame_num in np.where(thresholded_anom_results > 0)[0]]
+                destination_frames = [os.path.join(destination_dir, 'image-%06d' % frame_num + '.png') for frame_num in np.where(thresholded_anom_results > 0)[0]]
+                for src, dest in zip(anomalous_frames, destination_frames):
+                    import shutil;
+                    shutil.copyfile(src, dest)
+                
 
         except Exception as exc:
             print(exc)
             print('continuing...')
-
